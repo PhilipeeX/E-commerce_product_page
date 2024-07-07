3ebec6dd50d236653086208eb2b2bb920fe8382b
PhilipeX <dev.philipe@outlook.com>
2024-07-07 18:07:43 -0300

    feat(Cart): Adds more interactivity with buttons related to cart

--- js/cart_behavior.js
+++ js/cart_behavior.js
@@ -35,12 +35,18 @@ cartBtn.addEventListener("click", () => {
 
 sumBtn.addEventListener("click", () => {
   let quantity = Number(productQuantity.innerText);
+  if (quantity === 0){
+    addItemToCartBtn.classList.toggle('inactive')
+  }
   productQuantity.innerText = quantity + 1
 })
 
 minusBtn.addEventListener("click", () => {
   let quantity = Number(productQuantity.innerText);
-  if (Number(productQuantity.innerText) > 0){
+  if (quantity > 0){
+    if (quantity === 1){
+      addItemToCartBtn.classList.toggle('inactive')
+    }
     productQuantity.innerText = quantity - 1
   }
 })
@@ -50,11 +56,12 @@ addItemToCartBtn.addEventListener("click", () => {
   let blankCartMsg = cartElementBody.querySelector('p');
   const productImage = document.querySelector('.product-image').src
   const productDescription = document.querySelector('.description-text > h2').innerText
-  const productPrice = document.querySelector('.current-price').innerText
+  const productPrice = document.querySelector('.current-price').innerText.slice(1,-1)
   const cartBodyProduct = document.querySelector('#cart-body-filled')
   const productTitles = document.querySelectorAll('.product-thumb-description > span:first-of-type')
   const checkoutBtn = document.querySelector('.checkout')
   let newCartProduct = document.createElement("div")
+  let finalPriceCalculated = Number(productPrice)
   
   if (quantity !== 0){
     
@@ -69,7 +76,13 @@ addItemToCartBtn.addEventListener("click", () => {
         </div>
         <div class="product-thumb-description">
           <span>${productDescription}</span>
-          <span>${productPrice} x <span class="cart-product-quantity">1</span> <span class="final-price">$375.00</span></span>
+          <div>
+            <div class="thumb-price">
+              <span>${productPrice} x </span>
+              <span class="cart-product-quantity">1</span>
+            </div>
+            <span>$<span class="final-price">110.00</span></span>
+          </div>
         </div>
         <div class="product-thumb-discard">
           <svg width="14" height="16" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><defs><path d="M0 2.625V1.75C0 1.334.334 1 .75 1h3.5l.294-.584A.741.741 0 0 1 5.213 0h3.571a.75.75 0 0 1 .672.416L9.75 1h3.5c.416 0 .75.334.75.75v.875a.376.376 0 0 1-.375.375H.375A.376.376 0 0 1 0 2.625Zm13 1.75V14.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 1 14.5V4.375C1 4.169 1.169 4 1.375 4h11.25c.206 0 .375.169.375.375ZM4.5 6.5c0-.275-.225-.5-.5-.5s-.5.225-.5.5v7c0 .275.225.5.5.5s.5-.225.5-.5v-7Zm3 0c0-.275-.225-.5-.5-.5s-.5.225-.5.5v7c0 .275.225.5.5.5s.5-.225.5-.5v-7Zm3 0c0-.275-.225-.5-.5-.5s-.5.225-.5.5v7c0 .275.225.5.5.5s.5-.225.5-.5v-7Z" id="a"/></defs><use fill="#C3CAD9" fill-rule="nonzero" xlink:href="#a"/></svg>
@@ -89,7 +102,9 @@ addItemToCartBtn.addEventListener("click", () => {
       if (productTitles[i].innerText === productDescription) {
         let productQuantity = Number(productTitles[i].parentElement.querySelector('.cart-product-quantity').innerText)
         productQuantity += quantity
+        finalPriceCalculated = productQuantity * Number(productPrice)
         productTitles[i].parentElement.querySelector('.cart-product-quantity').innerText = productQuantity
+        productTitles[i].parentElement.querySelector('.final-price').innerText = finalPriceCalculated.toFixed(2)
         break
       } else {
         newCartProduct.classList.add('product-thumb')
@@ -101,7 +116,7 @@ addItemToCartBtn.addEventListener("click", () => {
           </div>
           <div class="product-thumb-description">
             <span>${productDescription}</span>
-            <span>${productPrice} x <span class="cart-product-quantity">1</span> <span class="final-price">$375.00</span></span>
+            <span>${productPrice} x <span class="cart-product-quantity">1</span> <span class="final-price">$110.00</span></span>
           </div>
           <div class="product-thumb-discard">
             <svg width="14" height="16" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><defs><path d="M0 2.625V1.75C0 1.334.334 1 .75 1h3.5l.294-.584A.741.741 0 0 1 5.213 0h3.571a.75.75 0 0 1 .672.416L9.75 1h3.5c.416 0 .75.334.75.75v.875a.376.376 0 0 1-.375.375H.375A.376.376 0 0 1 0 2.625Zm13 1.75V14.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 1 14.5V4.375C1 4.169 1.169 4 1.375 4h11.25c.206 0 .375.169.375.375ZM4.5 6.5c0-.275-.225-.5-.5-.5s-.5.225-.5.5v7c0 .275.225.5.5.5s.5-.225.5-.5v-7Zm3 0c0-.275-.225-.5-.5-.5s-.5.225-.5.5v7c0 .275.225.5.5.5s.5-.225.5-.5v-7Zm3 0c0-.275-.225-.5-.5-.5s-.5.225-.5.5v7c0 .275.225.5.5.5s.5-.225.5-.5v-7Z" id="a"/></defs><use fill="#C3CAD9" fill-rule="nonzero" xlink:href="#a"/></svg>
